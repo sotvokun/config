b6f2319d501c39a6711a1c0137643cced3eaed34
sotvokun <sotvokun@outlook.com>
2024-04-03 22:39:00 +0800

    emacs: something setups

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -3,3 +3,12 @@
 
 ; Change theme
 (load-theme 'modus-operandi)
+
+; Disable backup files
+(setq make-backup-files nil)
+
+; Disable auto-save files
+(setq auto-save-default nil)
+
+; Disable blinking cursor
+(blink-cursor-mode 0)
