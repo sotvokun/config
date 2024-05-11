a0608bd90b7146293281f12cb69e2d9b872841de
sotvokun <sotvokun@outlook.com>
2024-05-11 10:18:20 +0800

    powershell: no longer to need another Windows-related files

--- Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,39 @@
-. "${env:USERPROFILE}\.pwshrc"
+# PSReadLine
+Import-Module PSReadLine
+Set-PSReadLineOption -EditMode Emacs -BellStyle Visual
+
+
+# Alias
+Set-Alias -Name open -Value Explorer.exe
+
+
+# Prompt
+function prompt
+{
+	$cmd = "${env:USERNAME}@${env:COMPUTERNAME}"
+	Write-Host "${env:USERNAME}@${env:COMPUTERNAME} " -NoNewline -ForegroundColor White
+	Write-Host (Get-Location).Path.Replace($env:USERPROFILE, '~') -NoNewline -ForegroundColor Green
+	Write-Host "`n>" -NoNewline
+	return ' '
+}
+
+
+# Junction 
+(@{	#Path #Target
+	"${env:LOCALAPPDATA}\nvim" = "${env:USERPROFILE}\.config\nvim"
+}).GetEnumerator() | ForEach-Object {
+	if (!(Test-Path "$($_.Key)"))
+	{
+		[void](New-Item -ItemType Junction -Path "$($_.Key)" -Target "$($_.Value)")
+	}
+}
+
+
+# Local Profile
+$local_path = "$(Split-Path -Parent $PROFILE)\Microsoft.PowerShell_profile.local.ps1"
+if (Test-Path $local_path)
+{
+	. "${local_path}"
+}
+
+# vim: ft=ps1 ts=4
