295982025e55a9171af9110628ec1de457b01c05
sotvokun <sotvokun@outlook.com>
2024-10-26 15:37:41 +0800

    setup: git-credential-manager for wsl

--- setup.sh
+++ setup.sh
@@ -24,3 +24,15 @@ cp -rf .config "$HOME/"
 if [[ "$(uname)" == 'Windows_NT' && ! -h "$HOME/AppData/Local/nvim" ]]; then
 	ln -s "$HOME/.config/nvim" "$HOME/AppData/Local/nvim"
 fi
+
+# wsl setup
+if [[ "$(uname)" == 'Linux' ]]; then
+	if [[ $(grep -i Microsoft /proc/version) ]]; then
+		/mnt/c/Windows/System32/where.exe /Q git-credential-manager
+		if [[ $? == 0 ]]; then
+			gcm_win_path=$(/mnt/c/Windows/System32/where.exe git-credential-manager)
+			gcm_path=$(echo $gcm_win_path | sed 's|\\|/|g; s|C:|/mnt/c|' | sed 's/ /\\ /g')
+			git config --global credential.helper "$gcm_path"
+		fi
+	fi
+fi
