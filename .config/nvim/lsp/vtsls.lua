78c88b8b806636a3cdfdd6139e37cc0c816a7233
sotvokun <sotvokun@outlook.com>
2026-06-13 22:16:12 +0800

    nvim: add vue-language-server support

--- .config/nvim/lsp/vtsls.lua
+++ .config/nvim/lsp/vtsls.lua
@@ -2,8 +2,7 @@
 --
 
 local EXEC = 'vtsls'
-
-return vim.lsp.define_config(EXEC, {
+local exec_config = {
 	cmd = { 'vtsls', '--stdio' },
 	filetypes = {
 		"javascript",
@@ -29,6 +28,32 @@ return vim.lsp.define_config(EXEC, {
 			enableMoveToFileCodeAction = true,
 		},
 	},
-})
+}
+
+-- for vue-language-server typescript-plugin
+-- REFERENCE: https://github.com/vuejs/language-tools/wiki/Neovim
+local VUELS_EXEC = 'vue-language-server'
+local vuels_exists, from_mason, mason_package = vim.lsp.executable(VUELS_EXEC)
+if vuels_exists and from_mason then
+	table.insert(exec_config.filetypes, 'vue')
+	local vuels_module_path = vim.fs.joinpath(
+		mason_package:get_install_path(),
+		'node_modules',
+		'@vue',
+		'language-server'
+	)
+	exec_config.settings.vtsls.tsserver = {
+		globalPlugins = {
+			{
+				name = '@vue/typescript-plugin',
+				location = vuels_module_path,
+				languages = { 'vue' },
+				configNamespace = 'typescript'
+			}
+		}
+	}
+end
+
+return vim.lsp.define_config(EXEC, exec_config)
 
 -- vim: ts=4
